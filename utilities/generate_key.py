e4b267f84d9bf609b7141d1553f2bad178e6fc2e
Benito Vargas <benitognet@gmail.com>
2023-08-24 22:20:54 -0400

    Printing environment key

--- utilities/generate_key.py
+++ utilities/generate_key.py
@@ -3,6 +3,7 @@ from base64 import b64decode
 
 def main(json_file_name):
     key = os.environ.get('SERVICE_ACCOUNT_KEY')
+    print("Key from environment:", key)
     
     with open(json_file_name, 'w+') as json_file:
         json_file.write(b64decode(key).decode())
